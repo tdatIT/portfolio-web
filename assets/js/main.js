9a00ed116715d62242ce8a8c3d596ec7425c5eb4
dattt <dattt@ttcagris.com.vn>
2026-03-18 07:17:46 +0000

    #update: profile information, replace project section with experience timeline, and change CV download link

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,12 +24,11 @@ $("#confirm-btn").on('click', function () {
     if (input === 'hello') {
         $("#confirm-input").val("")
         link = document.createElement("a");
-        link.setAttribute("href", "/assets/pdf/CV-Backend-TienDat-01-2024.pdf");
-        link.setAttribute("download", "CV-Backend-TienDat-01-2024.pdf");
+        link.setAttribute("href", "/assets/pdf/03_2026_Backend_TienDat.pdf");
+        link.setAttribute("download", "CV_Backend_TienDat_03_2026.pdf");
         link.click();
         link.remove()
         MicroModal.close('modal-2')
     }
 })
-
 
