e9efb3e69b13eac8e2e60038333a252793513d15
Tien Dat <tdat.it2k2@gmail.com>
2026-03-27 00:41:24 +0700

    Fix: update profile experience number to remove tilde and adjust download link in JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,8 +23,8 @@ $("#confirm-btn").on('click', function () {
     let input = $("#confirm-input").val()
     if (input === 'hello') {
         $("#confirm-input").val("")
-        link = document.createElement("a");
-        link.setAttribute("href", "/assets/pdf/03_2026_Middle_Backend_TranTienDat.pdf");
+        const link = document.createElement("a");
+        link.setAttribute("href", "assets/pdf/03_2026_Middle_Backend_TranTienDat.pdf");
         link.setAttribute("download", "TienDat_Mid_Backend_03_2026.pdf");
         link.click();
         link.remove()
